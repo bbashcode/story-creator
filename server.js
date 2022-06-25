ecd282cccfa2cbeef287c6d0065331d6b1fba780
siopao2001 <sio_pao2001@yahoo.ca>
2022-06-25 20:56:47 +0000

    fixed bug with routes

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
