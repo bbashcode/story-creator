d93b224fbb4c80e36ece8ffc17ad704f817a8a51
siopao2001 <sio_pao2001@yahoo.ca>
2022-06-25 14:24:34 +0000

    Modify home page format and db query to show list of stories

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -27,13 +27,13 @@ module.exports = (db) => {
     //       .status(500)
     //       .json({ error: err.message });
     //   });
-       Promise.all([db.query(`SELECT * FROM users WHERE id = $1;`, [userId]), db.query(`SELECT * FROM stories;`)])
+       Promise.all([db.query(`SELECT * FROM users WHERE id = $1;`, [userId]), db.query(`SELECT stories.*, users.username AS author FROM stories JOIN users ON stories.creator_id = users.id ORDER BY created_at;`)])
        .then((values) => {
             if(values[0].rows === 0) {
               res.send({error: "no user with that id"});
               return;
              }
-            const templateVars = {user: values[0].rows[0], stories: values[1].rows[0]};
+            const templateVars = {user: values[0].rows[0], stories: values[1].rows};
             res.render('home', templateVars);
         })
   });
